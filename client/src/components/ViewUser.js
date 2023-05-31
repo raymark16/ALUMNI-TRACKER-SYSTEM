ddf8c7e89260eb20014d16abb337dfcdef912d83
raymark16 <cruzraymark16@yahoo.com>
2023-05-31 10:09:03 +0800

    view user design

--- client/src/components/ViewUser.js
+++ client/src/components/ViewUser.js
@@ -30,7 +30,7 @@ const ViewUser = () => {
         <div className="wrapper2 fadeIn_Down">
         <h1 className='fadeIn first font-weight-bold'>User Information</h1>
             <div id="form_content" className='pt-4'>
-                <div className='d-flex'>
+                <div className='d-flex justify-content-center'>
             <img src={uploadedPicture} className='fadeIn first rounded' alt='image' width='150px' height='150px'></img>
             <div className='d-flex flex-column justify-content-center ms-2'>
             <h1 className='fadeIn first'>{`${userInfo?.firstname} ${userInfo?.lastname}`}</h1>
@@ -38,19 +38,23 @@ const ViewUser = () => {
             </div>
             </div>
             <hr></hr>
-                <div className='d-flex flex-column align-items-center'>
-                    <h1 className='fadeIn first'>About me</h1>
-                    <div className='d-flex flex-column ms-20'>
-                        <h5 className='fadeIn first'>phone: {userInfo?.phone}</h5>
-                        <h5 className='fadeIn first'>email: {userInfo?.email}</h5>
-                        <h5 className='fadeIn first'>date graduated: {userInfo?.date_graduated}</h5>
-                        <div className='d-flex justify-content-center'>
-                        <h5 className='fadeIn first'>program: </h5>
-                        <h5 className='fadeIn first text-start' style={{fontSize:'20px'}}>{userProgram}</h5>
-                        </div>
-                    </div>
+            <h1 className='fadeIn first mb-3'>About me</h1>
+            <div className='d-flex justify-content-center'>
+                <div className='d-flex flex-column align-items-end me-3' style={{width:'35%'}}>
+                    <h5 className='fadeIn first'>Phone:</h5>
+                    <h5 className='fadeIn first'>Email:</h5>
+                    <h5 className='fadeIn first'>Date graduated:</h5>
+                    <h5 className='fadeIn first'>Program: </h5>
                 </div>
-                <input type='button' style={{margin: '30px 5px 5px 5px',width: '85%', padding: '15px 32px', display: 'inline-block'}} className="fadeIn fifth" onClick={() => navigate('/update-user')} value="Update Profile"/>
+                <div className='d-flex flex-column w-50'>
+                <h5 className='fadeIn first text-start ms-2'>{userInfo?.phone}</h5>
+                <h5 className='fadeIn first text-start ms-2'> {userInfo?.email}</h5>
+                <h5 className='fadeIn first text-start ms-2'>{userInfo?.date_graduated}</h5>
+                <h5 className='fadeIn first text-start ms-2' style={{fontSize:'20px'}}>{userProgram}</h5>
+                </div>
+            </div>
+                
+                <input type='button' style={{margin: '100px 5px 5px 5px',width: '25%', padding: '15px 32px', display: 'inline-block'}} className="fadeIn fifth" onClick={() => navigate('/update-user')} value="Update Profile"/>
             </div>
         </div>
   )
